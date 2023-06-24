abe79a849438586cdd91543404d952143b28551c
Kyle Slugg <kyle.slugg@gmail.com>
2023-06-24 16:41:59 -0400

    Reorganized client components

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -27,7 +27,6 @@ const App = () => {
     <MapWrapper center={startingCenter} zoom={zoom}>
       <Layers />
       <Locator>
-        <GenerateWalkingPath />
         <FeatureSaveLoad />
       </Locator>
       <MapControls />
